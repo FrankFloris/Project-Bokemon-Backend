51bc7d1e80fc126b315746a1e8318b86e355986e
AriaHV <aria.heijmensvisser@gmail.com>
2019-02-11 11:55:00 +0100

    Added newly initialized sprint-boot project.

---
+++ bokemon-app/src/main/java/com/example/bokemonapp/BokemonAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.bokemonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokemonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BokemonAppApplication.class, args);
+	}
+
+}
+
